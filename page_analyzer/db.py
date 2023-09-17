26f8bc101f3ac946d18f599717e8aefce09a0930
SergeiNaum <sergei.naumencko@yandex.ru>
2023-09-17 12:16:28 +0300

    before pushing to ruview

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,8 +1,10 @@
 import os
+from collections import namedtuple
+
 import psycopg2
 
 from datetime import datetime
-from typing import NamedTuple
+from typing import NamedTuple, List
 from psycopg2.extras import NamedTupleCursor
 from dotenv import load_dotenv
 
@@ -62,20 +64,39 @@ class FDataBase:
         except psycopg2.DatabaseError as e:
             print('Ошибка create_url_check' + ' ' + str(e))
 
-    def get_urls_and_last_checks_data(self) -> NamedTuple:
+    def get_urls_and_last_checks_data(self) -> List[NamedTuple]:
         try:
-            self.__cur.execute("""SELECT DISTINCT ON (urls.id)
-                        urls.id,
-                        urls.name,
-                        url_checks.status_code,
-                        url_checks.created_at
-                        FROM urls
-                        LEFT JOIN url_checks
-                        ON urls.id = url_checks.url_id
-                        ORDER BY urls.id DESC, url_checks.created_at DESC;""")
-            data = self.__cur.fetchall()
+            # self.__cur.execute("""SELECT DISTINCT ON (urls.id)
+            #             urls.id,
+            #             urls.name,
+            #             url_checks.status_code,
+            #             url_checks.created_at
+            #             FROM urls
+            #             LEFT JOIN url_checks
+            #             ON urls.id = url_checks.url_id
+            #             ORDER BY urls.id DESC, url_checks.created_at DESC;""")
+            # data = self.__cur.fetchall()
+            self.__cur.execute(
+                """SELECT DISTINCT ON (urls.id) id, name FROM urls;"""
+            )
+            data_urls = self.__cur.fetchall()
+            self.__cur.execute(
+                """SELECT DISTINCT ON (url_checks.url_id) url_id, status_code, created_at
+                FROM url_checks;"""
+            )
+            data_url_checks = self.__cur.fetchall()
+            data_urls_dict = {url.id: url for url in data_urls}
+            record = namedtuple('Record', ['id', 'name', 'status_code', 'created_at'])
+
+            data = []
+            for check in data_url_checks:
+                url = data_urls_dict.get(check.url_id)
+                if url:
+                    data.append(record(url.id, url.name, check.status_code, check.created_at))
+            data = sorted(data, key=lambda rec: (rec.id, rec.created_at), reverse=True)
 
             return data
+
         except psycopg2.DatabaseError as e:
             print('Ошибка get_urls_and_last_checks_data' + ' ' + str(e))
 

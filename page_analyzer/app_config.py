f99bfc103814777b9b6ab695e20c16a0b0948103
SergeiNaum <sergei.naumencko@yandex.ru>
2023-09-26 17:55:00 +0300

    fixing code after code review

--- page_analyzer/app_config.py
+++ page_analyzer/app_config.py
@@ -1,7 +1,11 @@
 import os
 from dotenv import load_dotenv
+from polog import config, file_writer
 
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+conf = config.add_handlers(file_writer('app_logs'))
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from polog import log
 
 import psycopg2
 
@@ -6,31 +7,16 @@ from datetime import datetime
 from typing import NamedTuple, List
 from psycopg2.extras import NamedTupleCursor
 
-from page_analyzer.app_config import DATABASE_URL
 
-
+@log
 def get_connection(db_url):
     return psycopg2.connect(db_url)
 
 
-def create_db_connection(database_url):
-    def decorator(func):
-        def wrapper(*args):
-            db = get_connection(database_url)
-            cur = db.cursor(cursor_factory=NamedTupleCursor)
-            try:
-                return func(*args, cur, db)
-            except psycopg2.DatabaseError as e:
-                print(f'Ошибка соединения с бд: \n {e}')
-            finally:
-                cur.close()
-                db.close()
-        return wrapper
-    return decorator
-
-
-@create_db_connection(DATABASE_URL)
-def add_url(url_name: str, cur: psycopg2, db: psycopg2) -> NamedTuple:
+@log
+# @create_db_connection(DATABASE_URL)
+def add_url(url_name: str, db: psycopg2) -> NamedTuple:
+    cur = db.cursor(cursor_factory=NamedTupleCursor)
     try:
         cur.execute(
             """INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;""",
@@ -41,11 +27,13 @@ def add_url(url_name: str, cur: psycopg2, db: psycopg2) -> NamedTuple:
 
         return id.id
     except psycopg2.DatabaseError as e:
-        print('Ошибка add_url' + ' ' + str(e))
+        raise Exception("An error occurred while adding the URL") from e
 
 
-@create_db_connection(DATABASE_URL)
-def create_url_check(url, status_code: int, tags_data: dict, cur: psycopg2, db: psycopg2):
+@log
+# @create_db_connection(DATABASE_URL)
+def create_url_check(url, status_code: int, tags_data: dict, db: psycopg2):
+    cur = db.cursor(cursor_factory=NamedTupleCursor)
     try:
         cur.execute(
             """INSERT INTO url_checks
@@ -62,11 +50,13 @@ def create_url_check(url, status_code: int, tags_data: dict, cur: psycopg2, db: 
         )
         db.commit()
     except psycopg2.DatabaseError as e:
-        print('Ошибка create_url_check' + ' ' + str(e))
+        raise Exception("An error occurred while creating URL check !") from e
 
 
-@create_db_connection(DATABASE_URL)
-def get_url_by_url_name(url_name: str, cur: psycopg2, db: psycopg2) -> NamedTuple:
+@log
+# @create_db_connection(DATABASE_URL)
+def get_url_by_url_name(url_name: str, db: psycopg2) -> NamedTuple:
+    cur = db.cursor(cursor_factory=NamedTupleCursor)
     try:
         cur.execute("""SELECT * FROM urls WHERE name = %s LIMIT 1;""", (url_name,), )
         url = cur.fetchone()
@@ -74,11 +64,13 @@ def get_url_by_url_name(url_name: str, cur: psycopg2, db: psycopg2) -> NamedTupl
         return url
 
     except psycopg2.DatabaseError as e:
-        print('Ошибка create_url_check' + ' ' + str(e))
+        raise Exception("An error occurred while getting URL by URL name !") from e
 
 
-@create_db_connection(DATABASE_URL)
-def get_urls_and_last_checks_data(cur: psycopg2, db: psycopg2) -> List[NamedTuple]:
+@log
+# @create_db_connection(DATABASE_URL)
+def get_urls_and_last_checks_data(db: psycopg2) -> List[NamedTuple]:
+    cur = db.cursor(cursor_factory=NamedTupleCursor)
     try:
 
         cur.execute(
@@ -89,13 +81,13 @@ def get_urls_and_last_checks_data(cur: psycopg2, db: psycopg2) -> List[NamedTupl
             """SELECT DISTINCT ON (url_checks.url_id) url_id, status_code, created_at
             FROM url_checks;"""
         )
-        data_url_checks = cur.fetchall()
-        data_urls_dict = {url.id: url for url in data_urls}
+        url_checks = cur.fetchall()
+        urls_dict = {url.id: url for url in data_urls}
         record = namedtuple('Record', ['id', 'name', 'status_code', 'created_at'])
 
         data = []
-        for check in data_url_checks:
-            url = data_urls_dict.get(check.url_id)
+        for check in url_checks:
+            url = urls_dict.get(check.url_id)
             if url:
                 data.append(record(url.id, url.name, check.status_code, check.created_at))
         data = sorted(data, key=lambda rec: (rec.id, rec.created_at), reverse=True)
@@ -103,23 +95,27 @@ def get_urls_and_last_checks_data(cur: psycopg2, db: psycopg2) -> List[NamedTupl
         return data
 
     except psycopg2.DatabaseError as e:
-        print('Ошибка get_urls_and_last_checks_data' + ' ' + str(e))
+        raise Exception("An error occurred while getting URLs & last checks !") from e
 
 
-@create_db_connection(DATABASE_URL)
-def get_url_by_id(url_id, cur: psycopg2, db: psycopg2) -> NamedTuple:
+@log
+# @create_db_connection(DATABASE_URL)
+def get_url_by_id(url_id, db: psycopg2) -> NamedTuple:
+    cur = db.cursor(cursor_factory=NamedTupleCursor)
     try:
-        cur.execute("""SELECT * FROM urls WHERE id = %s LIMIT 1;""", (url_id,), )
+        cur.execute('''SELECT * FROM urls WHERE id = %s LIMIT 1;''', (url_id,), )
         url = cur.fetchone()
 
         return url
 
     except psycopg2.DatabaseError as e:
-        print('Ошибка get_url_by_id' + ' ' + str(e))
+        raise Exception("An error occurred while getting URL by ID !") from e
 
 
-@create_db_connection(DATABASE_URL)
-def get_url_checks_by_url_id(url_id, cur: psycopg2, db: psycopg2) -> NamedTuple:
+@log
+# @create_db_connection(DATABASE_URL)
+def get_url_checks_by_url_id(url_id, db: psycopg2) -> NamedTuple:
+    cur = db.cursor(cursor_factory=NamedTupleCursor)
     try:
         cur.execute("""SELECT *
                 FROM url_checks
@@ -129,4 +125,4 @@ def get_url_checks_by_url_id(url_id, cur: psycopg2, db: psycopg2) -> NamedTuple:
         return url_checks
 
     except psycopg2.DatabaseError as e:
-        print('Ошибка get_url_checks_by_url_id' + ' ' + str(e))
+        raise Exception("An error occurred while getting URL checks by URL ID!") from e
